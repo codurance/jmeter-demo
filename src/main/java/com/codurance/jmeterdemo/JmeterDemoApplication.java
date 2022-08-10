445653ad5df8d9ffc00def92761cdba1d38888bd
Javier Chacana <javier.chacana@codurance.com>
2022-08-10 05:31:10 -0400

    Initial commit

---
+++ src/main/java/com/codurance/jmeterdemo/JmeterDemoApplication.java
@@ -0,0 +1,13 @@
+package com.codurance.jmeterdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmeterDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmeterDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codurance/jmeterdemo/JmeterDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codurance.jmeterdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmeterDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
